d93e7a858c7e4fdcb2b5d8324c764e046ae37f11
jay <jish3262@colorado.edu>
2023-07-21 09:56:31 -0600

    inv_pen

---
+++ inv_pend.jl
@@ -0,0 +1,66 @@
+using PRONTO
+using StaticArrays
+using LinearAlgebra
+
+@kwdef struct InvPend <: Model{2,1}
+    L::Float64 = 2 # length of pendulum (m)
+    g::Float64 = 9.81 # gravity (m/s^2)
+end
+
+@define_f InvPend begin
+[
+    x[2],
+    g/L*sin(x[1])-u[1]*cos(x[1])/L,
+]
+end
+
+@define_l InvPend 1/2*x'*I(2)*x + 1/2*u'*I(1)*u
+
+
+@define_m InvPend begin
+    P = [
+            1 0;
+            0 1;
+        ]
+    1/2*x'*P*x
+end
+
+@define_Q InvPend diagm([10, 1])
+@define_R InvPend diagm([1e-3])
+
+resolve_model(InvPend)
+
+##
+
+θ = InvPend() 
+τ = t0,tf = 0,10
+x0 = @SVector [2π/3;0]
+xf = @SVector [0;0]
+u0 = @SVector [0.0]
+
+α = t->xf
+μ = t->u0
+η = closed_loop(θ,x0,α,μ,τ)
+
+ξ,data = pronto(θ,x0,η,τ; maxiters=100, tol=1e-4);
+
+##
+
+using GLMakie
+
+fig = Figure()
+ts = 0:0.01:tf
+ax = Axis(fig[1,1]; xlabel="time [s]", ylabel="angle [rad]",title = "initial curve")
+x1 = [0*η.x(t)[1] for t∈ts]
+lines!(ax, ts, x1)
+
+ax = Axis(fig[2,1];xlabel="time [s]", ylabel="angular velocity [rad/s]")
+x2 = [0*η.x(t)[2] for t∈ts]
+lines!(ax, ts, x2)
+
+ax = Axis(fig[3,1]; xlabel="time [s]", ylabel="input [Nm]")
+u = [0*η.u(t)[1] for t∈ts]
+lines!(ax, ts, u)
+display(fig)
+
+save("initial_curve.png", fig, px_per_unit = 2)
