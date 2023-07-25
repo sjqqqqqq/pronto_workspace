fc4feb0d0769d423489fcac461ab7f163ffc64b8
jay <jish3262@colorado.edu>
2023-07-25 19:31:16 -0400

    different g

--- inv_pend.jl
+++ inv_pend.jl
@@ -32,7 +32,7 @@ resolve_model(InvPend)
 
 ##
 
-θ = InvPend() 
+θ = InvPend(g = 24.79) 
 τ = t0,tf = 0,10
 x0 = @SVector [2π/3;0]
 xf = @SVector [0;0]
@@ -64,3 +64,27 @@ lines!(ax, ts, u)
 display(fig)
 
 save("initial_curve.png", fig, px_per_unit = 2)
+
+##
+# using GLMakie
+
+n = Observable(1)
+fig = Figure()
+ax = Axis(fig[1,1], title = "inverted pendulum on jupiter",ylabel = "x1 [rad]")
+T = LinRange(τ...,10000)
+x1 = @lift [data.ξ[$n].x(t)[1] for t in T]
+lines!(ax, T, x1)
+ylims!(ax,-7,7)
+
+x2 = @lift [data.ξ[$n].x(t)[2] for t in T]
+ax = Axis(fig[2,1],ylabel = "x2 [rad/s]")
+lines!(ax, T, x2)
+ylims!(ax,-7,7)
+
+u1 = @lift [data.ξ[$n].u(t)[1] for t in T]
+ax = Axis(fig[3,1],xlabel="time [s]", ylabel="u [Nm]")
+lines!(ax, T, u1)
+ylims!(ax,-15,15)
+display(fig)
+
+record(x->n[]=x, fig, "test_jupiter.mp4", 1:length(data.ξ))
