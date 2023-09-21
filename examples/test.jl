0a5950fd78bb8efb854192beb6a34c2aebc31a7b
jay <jish3262@colorado.edu>
2023-09-21 15:00:01 -0600

    added 3D plot

--- examples/test.jl
+++ examples/test.jl
@@ -17,11 +17,12 @@ end
     
     H = Z + u[1]*X  # Hamiltonian 
     ρ = 1/2*(I(2) + x[1]*X + x[2]*Y + x[3]*Z)   # density matrix
+    ρ̇ = -im*(H*ρ-ρ*H)   # master equation
 
     [
-        real(tr(-im*(H*ρ-ρ*H)*X)),
-        real(tr(-im*(H*ρ-ρ*H)*Y)),
-        real(tr(-im*(H*ρ-ρ*H)*Z)),
+        real(tr(ρ̇*X)),
+        real(tr(ρ̇*Y)),
+        real(tr(ρ̇*Z)),
     ]  
  
 end
@@ -31,7 +32,8 @@ end
 end
 
 @define_m MixState begin
-    1/2*x'*x
+    xf = [0,0,-1]
+    1/2*(x-xf)'*(x-xf)
 end
 
 @define_Q MixState I(3)
@@ -39,3 +41,50 @@ end
 @define_R MixState I(1)
 
 resolve_model(MixState)
+
+PRONTO.Pf(θ::MixState,α,μ,tf) = SMatrix{3,3,Float64}(I(3))
+
+## ----------------------------------- solve the problem ----------------------------------- ##
+
+θ = MixState(kl=0.01)
+t0,tf = τ = (0,10)
+x0 = SVector{3}([0,0,1])
+μ = t->SVector{1}(0.2*sin(t))
+η = open_loop(θ,x0,μ,τ)
+ξ,data = pronto(θ,x0,η,τ;tol=1e-5,maxiters=50);
+
+## ----------------------------------- plot the results ----------------------------------- ##
+
+import Pkg
+Pkg.activate()
+using GLMakie
+GLMakie.activate!()
+
+function traj_on_sphere()
+
+    ts = range(t0,tf,length=1001);
+    
+    x1 = [ξ.x(t)[1] for t∈ts];
+    y1 = [ξ.x(t)[2] for t∈ts];
+    z1 = [ξ.x(t)[3] for t∈ts];
+
+    n = 100
+    u = range(-π,stop=π,length=n);
+    v = range(0,stop=π,length=n);
+
+    xs = cos.(u) * sin.(v)';
+    ys = sin.(u) * sin.(v)';
+    zs = ones(n) * cos.(v)';
+
+    aspect=(1, 1, 1)
+
+    fig = Figure()
+    ax = Axis3(fig[1, 1]; aspect) 
+    lines!(ax, x1, y1, z1; color=:red, linewidth=2)
+    surface!(ax, xs,ys,zs, rstride=4, cstride=4, transparency=true, overdraw=false)
+
+    display(fig)
+
+end
+
+traj_on_sphere()
