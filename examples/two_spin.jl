55e062fd4b1ccb2ee8996fd76e110dfe8b39b711
jay <jish3262@colorado.edu>
2023-10-17 13:15:48 -0600

    updated 2-spin

--- examples/two_spin.jl
+++ examples/two_spin.jl
@@ -1,118 +1,63 @@
 using PRONTO
-using StaticArrays
 using LinearAlgebra
+using StaticArrays
+using Base: @kwdef
 
-## --------------------- helper functions --------------------- ##
+## Two-level Qubit model
 
-function mprod(x)
-    Re = I(2)
-    Im = [0 -1;
-          1 0]
-    M = kron(Re,real(x)) + kron(Im,imag(x))
-    return M
+@kwdef struct Qubit <: Model{4,1}
+    kl::Float64 = 0.01
 end
 
-function inprod(x)
-    a = x[1:2]
-    b = x[3:4]
-    P = [a*a'+b*b' -(a*b'+b*a');
-         a*b'+b*a'   a*a'+b*b']
-    return P
+@define_f Qubit begin
+    H0 = [0 0 1 0;0 0 0 -1;-1 0 0 0;0 1 0 0]
+    H1 = [0 -1 0 0;1 0 0 0;0 0 0 -1;0 0 1 0]
+    (H0 + u[1]*H1)*x
 end
 
-## --------------------- modeling --------------------- ##
-@kwdef struct TwoSpin <: Model{4,1}
-    kr::Float64 = 1.0
-    kq::Float64 = 1.0
+@define_l Qubit begin
+    1/2*u'*kl*u
 end
 
-
-@define_f TwoSpin begin
-    H0 = [1 0;0 -1]
-    H1 = [0 1;1 0]
-    H2 = [0 -im;im 0]
-    mprod(-im*(H0+sin(u[1])*H1+(1-cos(u[1]))*H2))*x
-end
-
-@define_l TwoSpin 0.01/2*u'*u
-
-@define_m TwoSpin begin
+@define_m Qubit begin
     Pl = [1 0 0 0;0 0 0 0;0 0 1 0;0 0 0 0]
     1/2*x'*Pl*x
 end
 
-@define_Q TwoSpin kq*(I(4) - inprod(x))
-@define_R TwoSpin kr*I(1)
+@define_Q Qubit I(4)
+@define_R Qubit I(1)
+PRONTO.Pf(θ::Qubit, αf, μf, tf) = SMatrix{4,4,Float64}(I(4))
 
-PRONTO.Pf(θ::TwoSpin,α,μ,tf) = SMatrix{4,4,Float64}(I(4)-inprod(α))
+resolve_model(Qubit)
 
-# must be run after any changes to model definition
-resolve_model(TwoSpin)
+## Compute the optimal solution
 
-
-## --------------------- run optimization --------------------- ##
-
-θ = TwoSpin() # instantiate a new model
-τ = t0,tf = 0,5 # define time domain
+θ = Qubit() # instantiate a new model
+τ = t0,tf = 0,10 # define time domain
 x0 = @SVector [1.0, 0.0, 0.0, 0.0] # initial state
-μ = t->SVector{1}(0.5*sin(t)) # open loop input μ(t)
-η = open_loop(θ, x0, μ, τ); # guess trajectory
-@time ξ,data = pronto(θ, x0, η, τ;tol=1e-5); # optimal trajectory
+xf = @SVector [0.0, 1.0, 0.0, 0.0] # final state
+μ = t->SVector{1}(0.4*sin(t)) # open loop input μ(t)
+η = open_loop(θ, x0, μ, τ) # guess trajectory
+ξ,data = pronto(θ, x0, η, τ;tol=1e-4); # optimal trajectory
 
-## --------------------- outputs --------------------- ##
+## Plot the results
 
-using MAT
-
-ts = t0:0.001:tf
-us = [ξ.u(t)[1] for t∈ts]
-file = matopen("Uopt_2spin_10.mat","w")
-write(file,"Uopt",us)
-close(file)
-
-## --------------------- plots --------------------- ##
 using GLMakie
 
-dt = 0.01
-T = t0:dt:tf
-
 fig = Figure()
+ts = range(t0,tf,length=1001)
+ax1 = Axis(fig[1,1], xlabel = "time", ylabel = "quantum state")
+ax2 = Axis(fig[2,1], xlabel = "time", ylabel = "population")
+ax3 = Axis(fig[3,1], xlabel = "time", ylabel = "control input")
 
-sl = Slider(fig[3,1:2], range=1:18, startvalue=3)
-ix = sl.value
-
-ax = Axis(fig[1,1])
-ylims!(ax, (-1,1))
-
-for i in 1:4
-    x = @lift [data.ξ[$ix].x(t)[i] for t in T]
-    lines!(ax, T, x)
-end
-
-ax = Axis(fig[1,2])
-u = @lift [data.ξ[$ix].u(t)[1] for t in T]
-lines!(ax, T, u)
-
-
-ax = Axis(fig[2,1])
-# ylims!(ax, (-1,1))
-
-for i in 1:4
-    z = @lift [data.ξ[$ix].x(t)[i] + data.ζ[$ix].x(t)[i] for t in T]
-    lines!(ax, T, z)
-end
-
-ax = Axis(fig[2,2])
-u = @lift [data.ξ[$ix].u(t)[1] for t in T]
-lines!(ax, T, u)
-v = @lift [data.ξ[$ix].u(t)[1]+data.ζ[$ix].u(t)[1] for t in T]
-lines!(ax, T, v)
-
+lines!(ax1, ts, [ξ.x(t)[1] for t in ts], linewidth = 2, label = "Re(ψ1)")
+lines!(ax1, ts, [ξ.x(t)[2] for t in ts], linewidth = 2, label = "Re(ψ2)")
+lines!(ax1, ts, [ξ.x(t)[3] for t in ts], linewidth = 2, label = "Im(ψ1)")
+lines!(ax1, ts, [ξ.x(t)[4] for t in ts], linewidth = 2, label = "Im(ψ2)")
+fig[1, 2] = Legend(fig, ax1)
+lines!(ax2, ts, [ξ.x(t)[1]^2+ξ.x(t)[3]^2 for t in ts], linewidth = 2, label = "|0⟩")
+lines!(ax2, ts, [ξ.x(t)[2]^2+ξ.x(t)[4]^2 for t in ts], linewidth = 2, label = "|1⟩")
+fig[2, 2] = Legend(fig, ax2)
+lines!(ax3, ts, [ξ.u(t)[1] for t in ts], linewidth = 2)
 
 display(fig)
-
-
-
-##
-record(fig, "animated.mp4", 1:18) do jx
-    sl.value[] = jx
-end
