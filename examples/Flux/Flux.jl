b8a02fbdefa03bc71318466f9d6bb6056e5ea9c1
sjqqqqqq <jish3262@colorado.edu>
2023-12-13 18:05:21 -0700

    2 lvl flux model

--- examples/Flux/Flux.jl
+++ examples/Flux/Flux.jl
@@ -15,51 +15,51 @@ end
 
 ## Two-level Qubit model
 
-@kwdef struct Flux2Ex <: PRONTO.Model{9,1}
+@kwdef struct Flux2 <: PRONTO.Model{8,1}
     kl::Float64
     kr::Float64
     T::Float64 
 end
 
-@define_f Flux2Ex begin
+@define_f Flux2 begin
     E0 = 0.0
     E1 = 0.5725
     H0 = diagm([E0, E1])
     H00 = kron(I(2),H0)
     H1 = [0 -1im; 1im 0]
     H11 = kron(I(2),H1)
-    return [2 * π * mprod(-im * (H00 + x[9]*H11)) * x[1:8];u[1]]
+    return 2 * π * mprod(-im * (H00 + u[1]*H11)) * x
 end
 
-@define_l Flux2Ex begin
-    kl/2*u'*I*u + 1/2*max(kl,10*100^(-0.2*t),10*100^(0.2*(t-T)))*x[9]'*I*x[9]
+@define_l Flux2 begin
+    1/2*max(kl,10*100^(-0.2*t),10*100^(0.2*(t-T)))*u'*I*u
 end
 
-@define_m Flux2Ex begin
+@define_m Flux2 begin
     ψ1 = [1;0]
     ψ2 = [0;1]
-    xf = vec([ψ2;ψ1;0*ψ2;0*ψ1;0])
-    return 1/2*(x-xf)'*I(9)*(x-xf)
+    xf = vec([ψ2;ψ1;0*ψ2;0*ψ1])
+    return 1/2*(x-xf)'*I(8)*(x-xf)
 end
 
-@define_Q Flux2Ex I(9)
-@define_R Flux2Ex kr*I(1)
-PRONTO.Pf(θ::Flux2Ex,α,μ,tf) = SMatrix{9,9,Float64}(I(9))
+@define_Q Flux2 I(8)
+@define_R Flux2 kr*I(1)
+PRONTO.Pf(θ::Flux2,α,μ,tf) = SMatrix{8,8,Float64}(I(8))
 
-resolve_model(Flux2Ex)
+resolve_model(Flux2)
 
 ## Compute the optimal solution
 
-# θ = Flux2Ex(kl=0.03,kr=10,T=200)
-θ = Flux2Ex(kl=0.2,kr=10,T=600)
+# θ = Flux2(kl=0.2,kr=10,T=200) # 200ns optimal pulse
+θ = Flux2(kl=0.2,kr=10,T=600) # 600ns optimal pulse
 τ = t0,tf = 0,θ.T
 ψ1 = [1;0]
 ψ2 = [0;1]
-x0 = SVector{9}(vec([ψ1;ψ2;0*ψ1;0*ψ2;0]))
+x0 = SVector{8}(vec([ψ1;ψ2;0*ψ1;0*ψ2]))
 # μ = t->SVector{1}((π/tf)*exp(-(t-tf/2)^2/(tf^2))*sin(2*π*0.5725*t))
-μ = ξ.u
+μ = t->SVector{1}(0.001*sin(2*π*0.5725*t))
 η = open_loop(θ, x0, μ, τ) # guess trajectory
-ξ,data = pronto(θ, x0, η, τ;tol=1.2e-4); # optimal trajectory
+ξ,data = pronto(θ, x0, η, τ;tol=1e-6); # optimal trajectory
 
 ## Plot the results
 
@@ -71,7 +71,7 @@ ax1 = Axis(fig[1,1], xlabel = "time", ylabel = "control input")
 ax2 = Axis(fig[2,1], xlabel = "time", ylabel = "population")
 ax3 = Axis(fig[3,1], xlabel = "time", ylabel = "population")
 
-lines!(ax1, ts, [ξ.x(t)[9] for t in ts], linewidth = 2)
+lines!(ax1, ts, [ξ.u(t)[1] for t in ts], linewidth = 2)
 
 lines!(ax2, ts, [ξ.x(t)[1]^2+ξ.x(t)[5]^2 for t in ts], linewidth = 2, label = "|0⟩")
 lines!(ax2, ts, [ξ.x(t)[2]^2+ξ.x(t)[6]^2 for t in ts], linewidth = 2, label = "|1⟩")
@@ -88,7 +88,7 @@ using MAT
 
 dt = 0.1453218
 ts = 0:dt:tf
-us = [ξ.x(t)[end] for t in ts]
-file = matopen("Uopt_Flux_X_200ns.mat", "w")
+us = [ξ.u(t)[1] for t in ts]
+file = matopen("Uopt_Flux_X_600ns.mat", "w")
 write(file, "Uopt_X", us)
 close(file)
